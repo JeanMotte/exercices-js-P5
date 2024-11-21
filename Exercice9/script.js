3d9e0197da6d23f37931addd520aeb91defc357a
Jean Motte <jijimotte@gmail.com>
2024-11-21 15:47:10 +0100

    exercice 9

--- Exercice9/script.js
+++ Exercice9/script.js
@@ -1,3 +1,13 @@
 // votre code ici
 
 export default checkAge;
+
+function checkAge(age) {
+    if (Number(age) < 18) {
+        return "Vous êtes mineur.";
+    } else if (Number(age) >= 18 && Number(age) < 65) {
+        return "Vous êtes majeur.";
+    } else if (Number(age) >= 65) {
+        return "Vous êtes senior.";
+    }
+}
