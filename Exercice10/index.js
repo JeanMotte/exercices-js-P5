94b947be617172385c5fdf00d3b6952bf6e5eb53
Jean Motte <jijimotte@gmail.com>
2024-11-21 15:58:30 +0100

    add screenshots

--- Exercice10/index.js
+++ Exercice10/index.js
@@ -1,3 +1,11 @@
 //votre code ici
 
 export default printNumbers
+
+function printNumbers(n) {
+    let arr = []
+    for (let i = 1; i <= n; i++) {
+        arr.push(i)
+    }
+    return arr.join(' ')
+}
