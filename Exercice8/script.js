b273efcdfdb95ac1e4012ce0450f344e362bf7f5
Jean Motte <jijimotte@gmail.com>
2024-11-21 15:03:46 +0100

    Exercice 8

--- Exercice8/script.js
+++ Exercice8/script.js
@@ -1,3 +1,14 @@
 //Votre code ici
 
  export default add;
+
+ function add(a, b) {
+    if (
+        (typeof a === 'number' && !isNaN(a)) &&
+        (typeof b === 'number' && !isNaN(b))
+    ) {
+        return a + b;
+    } else {
+        throw new Error("Both a and b must be numbers (int or float).");
+    }
+}
